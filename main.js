3902034adb9d603f589d00a7cfb24aa68a0914d6
traycer255 <pavlyutenkovaleksey@yandex.ru>
2024-03-01 11:15:52 +0500

    Сделал динамический import слайдеру и поставил роутер

--- main.js
+++ main.js
@@ -1,25 +1,51 @@
-import 'normalize.css'
-import './style.scss'
+import 'normalize.css';
+import './style.scss';
+import Navigo from 'navigo';
 // import Swiper JS
-import { Navigation, Thumbs } from 'swiper/modules'
-import Swiper from 'swiper';
+// import { Navigation, Thumbs } from 'swiper/modules'
+// import Swiper from 'swiper';
 // import Swiper styles
-import 'swiper/css';
+// import 'swiper/css';
 
-const swiper = new Swiper(".product__slider-thumbnails", {
-  spaceBetween: 10,
-  slidesPerView: 4,
-  freeMode: true,
-  watchSlidesProgress: true,
-});
-const swiper2 = new Swiper(".product__slider-main", {
-  spaceBetween: 10,
-  navigation: {
-    nextEl: ".product__arrow_next",
-    prevEl: ".product__arrow_prev",
-  },
-  modules: [Navigation, Thumbs],
-  thumbs: {
-    swiper: swiper,
-  },
-});
+
+// изменения импортов.Динамический импорт:импорт произойдет при вызове функции слайдера
+const productSlider = () => {
+  // массив с промисами
+  Promise.all([
+    import('swiper/modules'),
+    import('swiper'),
+    import('swiper/css')
+  ]).then(([{ Navigation, Thumbs }, Swiper]) => {
+    console.dir(Swiper);
+    console.dir(Thumbs);
+    console.dir(Navigation);
+    // без default ошибка: swiper not constructor
+    const swiper = new Swiper.default(".product__slider-thumbnails", {
+      spaceBetween: 10,
+      slidesPerView: 4,
+      freeMode: true,
+      watchSlidesProgress: true,
+    });
+    // без default ошибка: swiper not constructor
+    const swiper2 = new Swiper.default(".product__slider-main", {
+      spaceBetween: 10,
+      navigation: {
+        nextEl: ".product__arrow_next",
+        prevEl: ".product__arrow_prev",
+      },
+      modules: [Navigation, Thumbs],
+      thumbs: {
+        swiper: swiper,
+      },
+    });
+  });
+
+};
+
+const init = () => {
+  productSlider();
+
+  const router = new Navigo('/', { linksSelector: 'a[href^="/"]' });
+};
+
+init();
